3458edf0edf664c64f7d837cb4721dc3f50cbb0f
TusharMinche <tusharminche@gmail.com>
2025-09-14 00:20:00 +0530

    Add ul tag

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Todo = ({ todoList, setTodoList }) => {
-  const [textInput, setTextInput] = useState('');
+  const [textInput, setTextInput] = useState("");
 
   const handleTextChange = (e) => {
     setTextInput(e.target.value);
-  }
+  };
 
   const handleAddBtn = () => {
-    if (textInput.trim() === '') return;
+    if (textInput.trim() === "") return;
     setTodoList([...todoList, textInput]);
-    setTextInput('');
-  }
+    setTextInput("");
+  };
 
   const handleDeleteBtn = (index) => {
     const temp = [...todoList];
     temp.splice(index, 1);
     setTodoList(temp);
-  }
+  };
 
   return (
     <div id="todo-container">
@@ -29,24 +29,28 @@ const Todo = ({ todoList, setTodoList }) => {
           value={textInput}
           placeholder="Enter a task..."
         />
-        <button id="add-btn" onClick={handleAddBtn}>Add Todo</button>
+        <button id="add-btn" onClick={handleAddBtn}>
+          Add Todo
+        </button>
       </div>
 
       <div className="todo-list">
-        {todoList.map((todo, index) => (
-          <div key={index} className="todo-item">
-            <p className="todo-text">{todo}</p>
-            <button
-              className="delete-btn"
-              onClick={() => handleDeleteBtn(index)}
-            >
-              Delete
-            </button>
-          </div>
-        ))}
+        <ul>
+          {todoList.map((todo, index) => (
+            <li key={index} className="todo-item">
+              <p className="todo-text">{todo}</p>
+              <button
+                className="delete-btn"
+                onClick={() => handleDeleteBtn(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
